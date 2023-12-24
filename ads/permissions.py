6e920ab110c0ca02cc9c6397aeb218e243a2c317
sergei valdemarov <valdemarovsergei@gmail.com>
2023-12-24 21:23:45 +0300

    Commit7

---
+++ ads/permissions.py
@@ -0,0 +1,29 @@
+from rest_framework.permissions import BasePermission
+
+from users.models import UserRole
+
+
+class IsAdmin(BasePermission):
+    '''
+    Доступ к просмотру для авторизированных пользователей с ролью ADMIN.
+    Доступ админа ко всем объектам.
+    '''
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.role == UserRole.ADMIN
+
+
+class IsOwner(BasePermission):
+    '''
+    Доступ к просмотру для авторизированных пользователей с ролью USER.
+    Доступ USER-а только к своему объекту (изменять чужие объекты нельзя).
+    '''
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
---
+++ ads/serializers.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+
+from .models import Ad, Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    # TODO сериалайзер для модели
+    pass
+
+
+class AdSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ad
+        fields = ['id', 'title', 'price', 'description', 'author', 'created_at']
+        read_only_fields = ['id', 'created_at', 'author']
+
+    def validate_price(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Цена не может быть отрицательной.")
+        return value
+
+
+class AdDetailSerializer(serializers.ModelSerializer):
+    '''
+    Сериализатор модели Ad(Объявления).
+    Детальный вид объявления.
+    Реализованы поля - имя, фамилия и электронная почта автора.
+    '''
+    # TODO сериалайзер для модели
+    author_first_name = serializers.CharField(source="author.first_name", read_only=True)
+    author_last_name = serializers.CharField(source="author.last_name", read_only=True)
+    author_email = serializers.CharField(source="author.email", read_only=True)
+
+    class Meta:
+        model = Ad
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    '''
+    Сериализатор модели Comment(Комментарии).
+    Реализованы поля - имя и фамилия автора.
+    '''
+    # TODO сериалайзер для модели
+    author_first_name = serializers.CharField(source="author.first_name", read_only=True)
+    author_last_name = serializers.CharField(source="author.last_name", read_only=True)
+
+    class Meta:
+        model = Comment
+        exclude = ('id', 'author')
